44c43da3a6675c5c50fe4986c27e30fec980b0f3
mcgraths7 <mcgrath.steven7@gmail.com>
2020-10-09 23:01:01 -0400

    Cleanup: fixed useForm hook
    Was referencing old code so copied a bit that was irrelevant

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -4,10 +4,7 @@ const useForm = (defaults) => {
   const [values, setValues] = useState(defaults);
 
   function updateValue(e) {
-    let { value } = e.target;
-    if (value.type === 'number') {
-      value = parseInt(e.target.value, 10);
-    }
+    const { value } = e.target;
     setValues({ ...values, [e.target.name]: value });
   }
 
