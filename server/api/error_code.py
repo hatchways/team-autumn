da2b1e18dc19f8d08915011d0b49c9c76ff81127
p <p@mail.com>
2020-10-15 17:51:17 -0400

    Use error code to indicate errors

---
+++ server/api/error_code.py
@@ -0,0 +1,5 @@
+PASSWORD_MISMATCH = -1  # Happens when register and login
+EMPTY_REQUIRED_FILED = -2  # Happens when register and login
+USER_EXIST = -3  # Happens when register
+USER_NOT_EXIST = -4  # Happens when login
+MIME_NOT_JSON = -5  # Happens when register and login
