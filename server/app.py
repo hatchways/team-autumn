d213b620605986dc4a43040118388d8c598d8406
p <p@mail.com>
2020-10-15 17:38:22 -0400

    add alias arg to db

--- server/app.py
+++ server/app.py
@@ -12,9 +12,10 @@ app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = os.environ.get('SECRET',
                                               b'VkO\xce\x0b\xc8\xde\xf1l\xaaQ+\x8bT\xfa\xbf\xb4\xf7\xdf\xc3OP\xcb\x86')
 default_db_addr = "localhost"  # Your own mongodb address here
+default_db_alias = "user-db"
 bcrypt.init_app(app)
 jwt.init_app(app)
-connect(os.environ.get("db_addr", default_db_addr))
+connect(os.environ.get("db_addr", default_db_addr), alias=os.environ.get("db_alias",default_db_alias))
 
 app.register_blueprint(home_handler)
 app.register_blueprint(ping_handler)
