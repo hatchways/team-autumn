54a0e806d9a54f61e467e370cc1844840b7377cf
p <p@mail.com>
2020-10-15 17:54:45 -0400

    Email is no longer the primary key; add class method to make User model fit OOP.

--- server/db/model.py
+++ server/db/model.py
@@ -12,13 +12,27 @@ class User(MongoModel):
         new_user=User("a@b.com",first_name="John",last_name="Smith",salted_password="aa-bb-cc-dd").save()
 
     """
-    email = fields.EmailField(primary_key=True)
+    email = fields.EmailField()
     first_name = fields.CharField()
     last_name = fields.CharField()
     salted_password = fields.CharField()
+
+    @staticmethod
+    def get_by_email(email):
+        ret = User.objects.raw({"email": email})
+        return list(ret)
+
+    def to_dict(self, remove_password=True):
+        ret = self.to_son().to_dict()
+        if "_cls" in ret:
+            del ret["_cls"]
+        if "_id" in ret:
+            del ret["_id"]
+        if remove_password:
+            del ret["salted_password"]
+        return ret
 
     class Meta:
         # This model will be used in the connection "user-db"
         connection_alias = 'user-db'
         ignore_unknown_fields = True
-
