200196df434c0e3a9a2c5fd768058fe26d624990
p <p@mail.com>
2020-10-15 17:33:03 -0400

    Use jsonschema to santize user input; Full register logic implemented

---
+++ server/api/register_handler.py
@@ -0,0 +1,54 @@
+import json
+from flask import jsonify, request, Blueprint, Response
+from db.model import User
+# Ref: https://medium.com/@riken.mehta/full-stack-tutorial-3-flask-jwt-e759d2ee5727
+from api import error_code
+from addon import bcrypt, jwt
+from api.util import get_schema, validate_json_input, fail_response
+from api.login_handler import login
+
+register_handler = Blueprint('register_handler', __name__)
+
+user_schema = {
+    # "type": "object",
+    "properties": {
+        "first_name": get_schema(),
+        "last_name": get_schema(),
+        "email": get_schema(format="email"),
+        "password": get_schema(minLength=6),
+        "confirm_password": get_schema(minLength=6)
+    },
+    "additionalProperties": False,
+    "minProperties": 5  # requires all 5 properties
+}
+
+
+@register_handler.route('/register', methods=['POST'])
+def register_entry():
+    """
+    API to handle register POST request.
+
+    """
+    if not request.is_json:
+        return fail_response(error_code.MIME_NOT_JSON), 400
+    err, user_json_str = validate_json_input(request.get_json(), user_schema)
+    if err:
+        print(err)
+        return fail_response(error_code.EMPTY_REQUIRED_FILED), 400
+    user_json = json.loads(user_json_str)
+    if user_json["password"] != user_json["confirm_password"]:
+        return fail_response(error_code.PASSWORD_MISMATCH)
+
+    # Assumption: user input is legit now
+
+    # Check if user exists
+    if User.get_by_email(user_json["email"]):
+        return fail_response(error_code.USER_EXIST)
+
+    # ADD TO db
+    salted_password = bcrypt.generate_password_hash(user_json["password"]).decode()
+    del user_json["password"]
+    del user_json["confirm_password"]
+    User(**user_json, salted_password=salted_password).save()
+    ret_json, _ = login()
+    return ret_json, 201
