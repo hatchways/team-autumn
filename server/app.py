80a873bb22a7d23d765e4a3f213ec089f2a1d7f6
p <p@mail.com>
2020-10-15 17:36:37 -0400

    add register blueprint to app. add flask extensions to app. add default value for testing.

--- server/app.py
+++ server/app.py
@@ -1,11 +1,21 @@
 from flask import Flask
 from api.ping_handler import ping_handler
 from api.home_handler import home_handler
-
+from api.register_handler import register_handler
+from addon import bcrypt, jwt
+from pymodm import connect
+import os
 
 app = Flask(__name__)
 
+# The secret is for test only
+app.config['JWT_SECRET_KEY'] = os.environ.get('SECRET',
+                                              b'VkO\xce\x0b\xc8\xde\xf1l\xaaQ+\x8bT\xfa\xbf\xb4\xf7\xdf\xc3OP\xcb\x86')
+default_db_addr = "localhost"  # Your own mongodb address here
+bcrypt.init_app(app)
+jwt.init_app(app)
+connect(os.environ.get("db_addr", default_db_addr))
 
 app.register_blueprint(home_handler)
 app.register_blueprint(ping_handler)
-
+app.register_blueprint(register_handler)
