fb0dbc4b1622a6bcf52b7ca4ea0e849a93405e47
p <p@mail.com>
2020-10-15 13:18:59 -0400

    Remove google_account_linked and sent_emails in User model Freeze library version with pipfile.lock

--- server/db/model.py
+++ server/db/model.py
@@ -1,16 +1,10 @@
 from pymodm import MongoModel, fields
 
 
-class Email(MongoModel):
-    """
-    Stub for future Email model
-    """
-    pass
-
-
 class User(MongoModel):
     """
     User model
+
 
     Example:
         from pymodm.connection import connect
@@ -22,11 +16,9 @@ class User(MongoModel):
     first_name = fields.CharField()
     last_name = fields.CharField()
     salted_password = fields.CharField()
-    # Google Oauth link status; Related data field might be added later
-    google_account_linked = fields.BooleanField(default=False)
-    sent_emails = fields.EmbeddedDocumentListField(Email)
 
     class Meta:
         # This model will be used in the connection "user-db"
         connection_alias = 'user-db'
+        ignore_unknown_fields = True
 
