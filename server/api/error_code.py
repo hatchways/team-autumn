6ac0dd311da1949fb1abcf884478a4e60557f687
p <p@mail.com>
2020-10-16 16:59:45 -0400

    update error code for descriptions

--- server/api/error_code.py
+++ server/api/error_code.py
@@ -4,3 +4,13 @@ USER_EXIST = -3  # Happens when register
 USER_NOT_EXIST = -4  # Happens when login
 MIME_NOT_JSON = -5  # Happens when register and login
 UNAUTHORIZED_ACCESS = -6  # Happens when login
+# WRONG_CREDENTIALS = -7 # TODO UNDER CONSIDER
+
+DESC = ["_",
+        "Password mismatch",
+        "Empty required field or input not match schema",
+        "User already exist",
+        "User does not exist",
+        "The 'Content-Type' is not set as 'application/json'",
+        "Unauthorized access",
+        ]
