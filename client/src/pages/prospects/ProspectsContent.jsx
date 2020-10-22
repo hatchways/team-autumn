1a7fdadacbd9cf2d17d4c8291bb65791aebbc65c
mcgraths7 <mcgrath.steven7@gmail.com>
2020-10-22 11:40:00 -0400

    added basic file upload with react-papaparse

--- client/src/pages/prospects/ProspectsContent.jsx
+++ client/src/pages/prospects/ProspectsContent.jsx
@@ -1,19 +1,28 @@
 import React, { useContext } from 'react';
-import { makeStyles } from '@material-ui/core';
+import { Button, makeStyles } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 
 import ProspectsContext from '../../contexts/ProspectsContext';
 import EnhancedDataTable from '../../components/EnhancedDataTable';
-import ComboButton from '../../components/ComboButton';
+import buttonStyles from '../../assets/styles/buttonStyles';
+import CsvUploadButton from '../../components/CsvUploadButton';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   mainGrid: {
     justifyContent: 'space-between',
     alignItems: 'center',
   },
+  buttonGrid: {
+    flexBasis: 'auto',
+    justifyContent: 'flex-end',
+    marginRight: theme.spacing(-4),
+  },
   faded: {
     color: '#9e9e9e',
+  },
+  centered: {
+    justifyContent: 'center',
   },
 }));
 
@@ -23,28 +32,28 @@ const testData = [
     email: 'steven@example.com',
     firstName: 'Steven',
     lastName: 'McGrath',
-    lastContacted: '7/27/2020',
+    status: 'open',
   },
   {
     id: 2,
     email: 'carrie@example.com',
     firstName: 'Carrie',
     lastName: 'Pascale',
-    lastContacted: '10/11/2020',
+    status: 'responded',
   },
   {
     id: 3,
     email: 'patton@example.com',
     firstName: 'Patton',
     lastName: 'L',
-    lastContacted: '9/13/2020',
+    status: 'responded',
   },
   {
     id: 4,
     email: 'shums@example.com',
     firstName: 'Shums',
     lastName: 'Kassam',
-    lastContacted: '9/04/2020',
+    status: 'unsubscribed',
   },
 ];
 
@@ -53,13 +62,12 @@ const headCells = [
   { id: 'email', numeric: false, disablePadding: false, label: 'Email' },
   { id: 'firstName', numeric: false, disablePadding: false, label: 'First Name' },
   { id: 'lastName', numeric: false, disablePadding: false, label: 'Last Name' },
-  { id: 'lastContacted', numeric: false, disablePadding: false, label: 'Last Contacted' },
+  { id: 'status', numeric: false, disablePadding: false, label: 'Status' },
 ];
-
-const options = ['Upload new prospect', 'Upload prospects from file'];
 
 const ProspectsContent = () => {
   const classes = useStyles();
+  const buttonClasses = buttonStyles();
   const [search] = useContext(ProspectsContext);
   const filteredData = testData.filter((d) => d.email.includes(search));
   return (
@@ -68,7 +76,18 @@ const ProspectsContent = () => {
         <Typography component="h3" variant="h6">
           Prospects
         </Typography>
-        <ComboButton ariaLabel="upload prospects" options={options} />
+        <Grid className={classes.buttonGrid} item container xs={6}>
+          <Grid item xs={4}>
+            <Button variant="contained" className={`${buttonClasses.base} ${buttonClasses.action}`}>
+              Add New Prospect
+            </Button>
+          </Grid>
+          <Grid className={classes.centered} container item xs={8}>
+            <Grid item xs={8}>
+              <CsvUploadButton />
+            </Grid>
+          </Grid>
+        </Grid>
       </Grid>
       <EnhancedDataTable
         className={classes.table}
