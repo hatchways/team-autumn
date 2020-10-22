65a21c5309a46eb6ea3d5be6fe6ba2227709bf89
p <p@mail.com>
2020-10-22 19:58:55 -0400

    New error_code for gmail auth Full gmail auth path added Gmail file move from ./auth to ./api

--- server/api/error_code.py
+++ server/api/error_code.py
@@ -4,7 +4,8 @@ USER_EXIST = -3  # Happens when register
 USER_NOT_EXIST = -4  # Happens when login
 MIME_NOT_JSON = -5  # Happens when register and login
 UNAUTHORIZED_ACCESS = -6  # Happens when login
-# WRONG_CREDENTIALS = -7 # TODO UNDER CONSIDER
+NO_VALID_GMAIL_OAUTH_CREDENTIALS = -7
+GMAIL_AUTH_FAILED = -8
 
 DESC = ["_",
         "Password mismatch",
@@ -13,4 +14,6 @@ DESC = ["_",
         "User does not exist",
         "The 'Content-Type' is not set as 'application/json'",
         "Unauthorized access",
+        "User's gmail oauth credentials is invalid",
+        "Error occurs when auth with google oauth server"
         ]
