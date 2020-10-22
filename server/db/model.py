3592e0f233cc110dc71a405e962c1c4870b39d52
p <p@mail.com>
2020-10-22 19:57:42 -0400

    Add Google_auth_info model to model.py; Add gmail related methods to User class

--- server/db/model.py
+++ server/db/model.py
@@ -1,4 +1,31 @@
 from pymodm import MongoModel, fields
+from google.auth.transport.requests import AuthorizedSession
+from google_auth_oauthlib.flow import Flow
+from google.oauth2.credentials import Credentials
+import json
+
+
+class GmailOauthInfo(MongoModel):
+    token = fields.CharField()
+    refresh_token = fields.CharField()
+    token_uri = fields.CharField()
+    client_id = fields.CharField()
+    client_secret = fields.CharField()
+    scopes = fields.ListField(field=fields.CharField())
+    expiry = fields.CharField()
+
+    def to_dict(self):
+        ret = self.to_son().to_dict()
+        if "_cls" in ret:
+            del ret["_cls"]
+        if "_id" in ret:
+            del ret["_id"]
+        return ret
+
+    class Meta:
+        # This model will be used in the connection "user-db"
+        connection_alias = 'user-db'
+        ignore_unknown_fields = True
 
 
 class User(MongoModel):
@@ -16,6 +43,7 @@ class User(MongoModel):
     first_name = fields.CharField()
     last_name = fields.CharField()
     salted_password = fields.CharField()
+    gmail_oauth_info = fields.EmbeddedDocumentField(GmailOauthInfo)
 
     @staticmethod
     def get_by_email(email):
@@ -32,17 +60,50 @@ class User(MongoModel):
         ret_list = list(ret)
         return ret_list[0] if ret_list else None
 
-    def to_dict(self, remove_password=True):
+    def update_credentials(self, cred):
+        if self.gmail_oauth_info and self.gmail_oauth_info.token == cred.token:
+            return
+        self.gmail_oauth_info = GmailOauthInfo(**json.loads(cred.to_json()))
+        self.save()
+
+    @property
+    def is_oauthed(self):
+        return self.gmail_oauth_info is not None
+
+    @property
+    def _session(self):
+        """
+
+        Returns:
+            AuthorizedSession
+        """
+        if not self.is_oauthed:
+            return None
+        authed_session = AuthorizedSession(Credentials.from_authorized_user_info(self.gmail_oauth_info.to_dict()))
+        return authed_session
+
+    def get_gmail_profile(self):
+        _token = self._session.credentials.token
+
+        res = self._session.get("https://gmail.googleapis.com/gmail/v1/users/me/profile?alt=json")
+        if self._session.credentials.token != _token:
+            self.save_credentials(self._session.credentials)
+        return res.text
+
+    def to_dict(self, remove_password=True, remove_oauth_info=True):
         ret = self.to_son().to_dict()
-        if "_cls" in ret:
-            del ret["_cls"]
-        if "_id" in ret:
-            del ret["_id"]
-        if remove_password:
-            del ret["salted_password"]
-        return ret
+        return {"email": self.email,
+                "first_name": self.first_name,
+                "last_name": self.last_name}
+
+    def user_info_dict(self):
+        return {"email": self.email,
+                "first_name": self.first_name,
+                "last_name": self.last_name}
 
     class Meta:
         # This model will be used in the connection "user-db"
         connection_alias = 'user-db'
         ignore_unknown_fields = True
+        # Save all referenced object when save is called on this.
+        # cascade = True
