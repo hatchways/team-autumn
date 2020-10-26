d4553be54a72abc329fd88da0d18e7e8591049a7
mcgraths7 <mcgrath.steven7@gmail.com>
2020-10-26 13:41:51 -0400

    added initial validation for existence of email address

--- client/src/pages/prospects/ProspectUpload.jsx
+++ client/src/pages/prospects/ProspectUpload.jsx
@@ -6,30 +6,38 @@ const EXPECTED_HEADERS = ['first_name', 'last_name', 'email'];
 
 const ProspectUpload = () => {
   const [loading, setLoading] = useState(false);
+  const [resultsText, setResultsText] = useState(false);
   const handleFileAdd = (newFile) => {
     setLoading(true);
     const { file } = newFile[0];
     const data = [];
+    const emails = [];
+    const errors = [];
+    let repeatEmails = 0;
     Papa.parse(file, {
       delimiter: ',',
-      transformHeader: (header) => header.trim(),
+      transformHeader: (header) => header.trim().toLocaleLowerCase(),
       skipEmptyLines: true,
       header: true,
       step: (results, parser) => {
         const headers = results.meta.fields;
-        if (!headers.includes('email')) {
-          console.log('Invalid format - email required. Aborting parse');
+        const emailHeader = headers.find((header) => header.includes('email'));
+        if (!emailHeader) {
+          errors.push('Invalid format - email required. Aborting parse');
           parser.abort();
+        } else if (emailHeader && emails.includes(results.data[emailHeader])) {
+          repeatEmails += 1;
         } else {
+          emails.push(results.data[emailHeader]);
           data.push(results.data);
         }
       },
       complete: () => {
-        console.log(data);
+        console.log(repeatEmails);
         setLoading(false);
       },
       error: (err) => {
-        console.log(err);
+        errors.push(err);
         setLoading(false);
       },
     });
