a9c49377ad19edfcf69af42d3706620bba725963
mcgraths7 <mcgrath.steven7@gmail.com>
2020-10-26 14:18:29 -0400

    added validation so that prospects cannot be uploaded without an email address

--- client/src/pages/prospects/ProspectUpload.jsx
+++ client/src/pages/prospects/ProspectUpload.jsx
@@ -1,18 +1,23 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { DropzoneAreaBase } from 'material-ui-dropzone';
+import DescriptionIcon from '@material-ui/icons/Description';
+import Typography from '@material-ui/core/Typography';
 import Papa from 'papaparse';
+
+import UserContext from '../../contexts/UserContext';
 
 const EXPECTED_HEADERS = ['first_name', 'last_name', 'email'];
 
 const ProspectUpload = () => {
+  const [user] = useContext(UserContext);
   const [loading, setLoading] = useState(false);
   const [resultsText, setResultsText] = useState(false);
   const handleFileAdd = (newFile) => {
     setLoading(true);
     const { file } = newFile[0];
-    const data = [];
     const emails = [];
     const errors = [];
+    const data = [];
     let repeatEmails = 0;
     Papa.parse(file, {
       delimiter: ',',
@@ -23,17 +28,40 @@ const ProspectUpload = () => {
         const headers = results.meta.fields;
         const emailHeader = headers.find((header) => header.includes('email'));
         if (!emailHeader) {
-          errors.push('Invalid format - email required. Aborting parse');
+          errors.push('Invalid format - email required');
           parser.abort();
         } else if (emailHeader && emails.includes(results.data[emailHeader])) {
           repeatEmails += 1;
         } else {
           emails.push(results.data[emailHeader]);
-          data.push(results.data);
+          data.push({
+            ...results.data,
+            email: results.data[emailHeader],
+            owner_email: user.email,
+          });
         }
       },
       complete: () => {
-        console.log(repeatEmails);
+        const prospectData = { ...data };
+        console.log(prospectData);
+        if (errors.length > 0) {
+          setResultsText(`There was a problem: ${errors[0]}`);
+        } else {
+          fetch('/upload_prospects', {
+            method: 'post',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(prospectData),
+          })
+            .then((response) => console.log(response))
+            .then((r) => console.log(r))
+            .catch((err) => console.log(err));
+
+          setResultsText(
+            `File parsed. ${data.length} prospects added. ${repeatEmails} duplicate(s) detected and ignored.`
+          );
+        }
         setLoading(false);
       },
       error: (err) => {
@@ -44,12 +72,18 @@ const ProspectUpload = () => {
   };
 
   return (
-    <DropzoneAreaBase
-      inputProps={{ accept: 'text/csv' }}
-      dropzoneText={loading ? 'Upload in progress...' : 'Click or drag a .csv file here'}
-      showFileNames
-      onAdd={handleFileAdd}
-    />
+    <>
+      <DropzoneAreaBase
+        inputProps={{ accept: 'text/csv' }}
+        dropzoneText={loading ? 'Upload in progress...' : 'Click or drag a .csv file here'}
+        showFileNames
+        onAdd={handleFileAdd}
+        Icon={DescriptionIcon}
+      />
+      <Typography align="center" variant="body2">
+        {resultsText}
+      </Typography>
+    </>
   );
 };
 export default ProspectUpload;
