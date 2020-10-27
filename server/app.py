57049aa145a398e69dfa2b03b2f0d8bda90a95a7
p <p@mail.com>
2020-10-27 11:52:48 -0400

    set new json_encoder

--- server/app.py
+++ server/app.py
@@ -1,13 +1,16 @@
-from flask import Flask,session
+from flask import Flask, session
 from api.ping_handler import ping_handler
 from api.home_handler import home_handler
 from api.register_handler import register_handler
 from api.auth_handler import auth_handler
 from api.gmail_auth_handler import gmail_auth_handler
+from api.campaign_handler import campaign_handler
 from addon import bcrypt, jwt
 from pymodm import connect
 import datetime
-import os
+import json
+from pymodm import MongoModel
+from bson import ObjectId
 
 app = Flask(__name__)
 app.config.from_object("config.Config")
@@ -22,9 +25,24 @@ app.register_blueprint(ping_handler)
 app.register_blueprint(register_handler)
 app.register_blueprint(auth_handler)
 app.register_blueprint(gmail_auth_handler)
+app.register_blueprint(campaign_handler)
 
 
 @app.before_request
 def make_session_permanent():
     session.permanent = True
     app.permanent_session_lifetime = datetime.timedelta(minutes=5)
+
+
+class ModelEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, MongoModel):
+            return obj.to_dict()
+        elif isinstance(obj, ObjectId):
+            return str(obj)
+        elif isinstance(obj, (datetime.date, datetime.datetime)):
+            return obj.isoformat()
+        return json.JSONEncoder.default(self, obj)
+
+
+app.json_encoder = ModelEncoder
